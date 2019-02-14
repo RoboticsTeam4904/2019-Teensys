b39be421eb4e24774fd81128b943d4a0be7cf048
System Administrator <root@nus11673-9-stehwan.nueva.local>
2019-02-14 15:58:40 -0800

    Updated Encoder Sender and Receiver, compatible with NeoPixel

---
+++ EncoderReceiver/EncoderReceiver.ino
@@ -0,0 +1,124 @@
+// Receiver for PCBs
+
+#include <FlexCAN.h>
+#include <Adafruit_NeoPixel.h>
+#include <TeensyCANBase.h>
+#include <string.h>
+
+#define PIN 7
+#define NUM_LEDS 30
+// Limit for Computer: 30 LEDs
+
+int i = 0;
+int toggle = 0;
+int themsg = 0;
+
+int lowerbound = -40;
+int upperbound = 40;
+
+//rainbow colors
+//int colors[9][3] = {
+//  {255, 0, 0}, //red
+//  {255, 127, 0},  //orange
+//  {255, 255, 0}, //yellow
+//  {0, 128, 0}, //dark green
+//  {0, 47, 181}, // nueva blue?
+//  {255, 0, 255}, //pink
+//  {75, 0, 130}, //dark purple
+//  {143, 0, 55} // light purple
+//};
+
+//red - blue gradient
+int colors[25][3] = {
+  {255, 0, 0},
+  {244, 0, 10},
+  {233, 0, 21},
+  {223, 0, 31},
+  {212, 0, 42},
+  {201, 0, 53},
+  {191, 0, 63},
+  {180, 0, 74},
+  {170, 0, 85},
+  {159, 0, 95},
+  {148, 0, 106},
+  {138, 0, 116},
+  {127, 0, 127},
+  {116, 0, 138},
+  {106, 0, 148},
+  {95, 0, 159},
+  {85, 0, 170},
+  {74, 0, 180},
+  {63, 0, 191},
+  {53, 0, 201},
+  {42, 0, 212},
+  {31, 0, 223},
+  {21, 0, 233},
+  {10, 0, 244},
+  {0, 0, 255}
+};
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+inline int16_t ReadINT16(uint8_t *ByteArray, int32_t Offset)
+{
+  int16_t result;
+  uint8_t* ptr1 = (uint8_t*)&result;
+  uint8_t* ptr2 = ptr1 + 1;
+  *ptr1 = *ByteArray;
+  *ptr2 = *(ByteArray + 1);
+  return result;
+}
+
+// gets the first number (of the two) in the message sent
+void checkCAN(byte* msg) {
+  themsg = ReadINT16(msg, 0);
+}
+
+// get the message
+int getMSG() {
+  return themsg;
+}
+
+void displayNeoPixel(int velocity) {
+  if (velocity > upperbound) {
+    velocity = upperbound;
+  } else if (velocity < lowerbound) {
+    velocity = lowerbound;
+  }
+  int range = upperbound - lowerbound;
+  int numcolors = sizeof(colors)/sizeof(colors[0]);
+  float scalefactor = (float)range / (float)numcolors;
+  int colorsindex = (velocity + (-lowerbound)) / scalefactor;
+  int *color = colors[colorsindex];
+  int r = color[0];
+  int g = color[1];
+  int b = color[2];
+  for (i = 0; i < NUM_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(r, g, b));
+    strip.show();
+    delay(10);
+  }
+}
+
+void setup(void) {
+  CAN_begin();
+  CAN_add_id(0x612, &checkCAN);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  strip.begin();
+  delay(1000);
+  Serial.println("Teensy 3.X CAN Receiver");
+  digitalWrite(13, HIGH);
+  toggle = 1;
+}
+void loop(void) {
+  CAN_update();
+  displayNeoPixel(getMSG());
+  if (toggle == 1) {
+    digitalWrite(13, LOW);
+    toggle = 0;
+  } else {
+    digitalWrite(13, HIGH);
+    toggle = 1;
+  }
+}
---
+++ EncoderSender/EncoderSender.ino
@@ -0,0 +1,87 @@
+// Sender for PCBs
+
+#include <Encoder.h>
+#include <FlexCAN.h>
+#include <TeensyCANBase.h>
+
+int pin1 = 7;
+int pin2 = 8;
+
+Encoder enc(pin1, pin2);
+
+float lastposition = 0;
+float lasttime = millis();
+int toggle = 0;
+int revolution = 1024;
+int interval = 50;
+int seconds = 1000;
+
+float encoderposition() {
+  float position = enc.read();
+  return position;
+}
+
+// velocity computed in rotations / second (1024 units / 1000 miliseconds)
+float encodervelocity() {
+  float currentposition = encoderposition();
+  float difference = currentposition - lastposition;
+  float dx = (float)difference / (float)revolution; // in revolutions
+  float dt = 1; // in seconds
+  float velocity = (float)dx / (float)dt;
+  return velocity;
+}
+
+void resetlastposition() {
+  float currentposition = encoderposition();
+  lastposition = currentposition;
+}
+
+void resetlasttime() {
+  float currenttime = millis();
+  lasttime = currenttime;
+}
+
+// writeLongs(id of receiver, msg 1, msg 2)
+void writeLongs(uint32_t id, long value1, long value2){
+  byte * msg = new byte[8];
+
+  for(int i = 0; i < 4; i++){
+    msg[i] = (value1 >> i*8) & 0xFF;
+  }
+  for(int i = 0; i < 4; i++){
+    msg[i + 4] = (value2 >> i*8) & 0xFF;
+  }
+  
+  CAN_write(id, msg);
+
+  delete msg;
+  if (toggle == 1){
+    digitalWrite(13, LOW);
+    toggle = 0;
+  } else {
+    digitalWrite(13, HIGH);
+    toggle = 1;
+  }
+}
+
+void setup(void) {
+  CAN_begin();
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  delay(1000);
+  Serial.println("Teensy 3.X CAN Sender");
+  digitalWrite(13, HIGH);
+  toggle = 1;
+}
+
+void loop() {
+  CAN_update();
+  if((int)(millis() - lasttime) % interval == 0) {
+    Serial.println(encodervelocity());
+    writeLongs(0x612, encodervelocity(), encoderposition());
+  }
+  if(millis() - lasttime >= seconds) {
+    resetlastposition();
+    resetlasttime();
+  }
+}
